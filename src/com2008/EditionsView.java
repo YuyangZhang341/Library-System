acbc1016e45a8b7d2d0aa7268e192b3c53a273cf
rmaciejewski <maciejewskirafal99@gmail.com>
2019-11-18 22:09:32 +0000

    Editions View completed except openButton

---
+++ src/com2008/EditionsView.java
@@ -0,0 +1,57 @@
+package com2008;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class EditionsView {
+    private JPanel backPanel;
+    private JScrollPane editionsScrollPane;
+    private JTable editionsTable;
+    private JPanel editionsPanel;
+    private JButton backButton;
+    private JButton openButton;
+
+    //TODO: set frame title to be the current journal's names with edition number
+    private static JFrame frame = new JFrame("Editions");
+
+    public EditionsView() {
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new VolumesView().showVolumesView();
+                frame.dispose();
+            }
+        });
+
+        openButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+//                String targetIssn = editionsTable.getValueAt(editionsTable.getSelectedRow(), 0).toString();
+            }
+        });
+    }
+
+    public void showEditionsView() {
+        frame.setContentPane(new EditionsView().editionsPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension screenDimensions = toolkit.getScreenSize();
+        frame.setSize(screenDimensions.width, screenDimensions.height);
+        frame.setVisible(true);
+    }
+
+
+    private void createUIComponents() {
+        editionsTable = new JTable(){
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            };
+        };
+
+        PublicationsController.fetchEditions(editionsTable);
+    }
+}
