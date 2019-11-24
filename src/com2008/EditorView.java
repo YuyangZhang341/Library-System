b3a214d82be0b2d26dce0ae1dbd91e3602e42842
Marcin Kiliszek <mkiliszek1@sheffield.ac.uk>
2019-11-24 20:55:09 +0000

    EditorView interface

---
+++ src/com2008/EditorView.java
@@ -0,0 +1,37 @@
+package com2008;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class EditorView {
+    private JPanel mainPanel;
+    private JPanel navPanel;
+    private JPanel contentPanel;
+    private JButton backButton;
+    private JButton changePasswordButton;
+    private JPanel articlesPanel;
+    private JPanel infoPanel;
+    private JTable articlesTable;
+    private JScrollPane tableScrollPane;
+    private JTextField titleTextField;
+    private JTextField ISSNTextField;
+    private JButton changeEditorButton;
+    private JButton publishButton;
+
+    private static JFrame frame = new JFrame("Article");
+
+    public static void showEditorView() {
+        frame.setContentPane(new EditorView().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension screenDimensions = toolkit.getScreenSize();
+        frame.setSize(screenDimensions.width, screenDimensions.height);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        showEditorView();
+    }
+}
