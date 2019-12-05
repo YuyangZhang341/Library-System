765d26c2648bc47d654e3d28d8ba847fe38f426b
Neo <gfeng1@sheffield.ac.uk>
2019-12-05 00:15:49 +0000

    enabled select and delete button.You can add no more than three articles form right JTable to left JTable

--- src/com2008/ReviewArticles.java
+++ src/com2008/ReviewArticles.java
@@ -1,6 +1,8 @@
 package com2008;
 
 import javax.swing.*;
+import javax.swing.event.AncestorEvent;
+import javax.swing.event.AncestorListener;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.*;
@@ -23,11 +25,17 @@ public class ReviewArticles {
     private JScrollPane rightJournalsScrollPane;
     private JTable leftTable;
     private JTable rightTable;
+    private JButton selectButton;
+    private JButton deleteButton;
+
+    private JTextField leftSubmissionIDTextField;
+    private JTextField leftTitleTextField;
+    private JTextField leftAbsTextField;
+
     private static JFrame frame = new JFrame("Review");
 
     public ReviewArticles(){
-        loadRightTable();
-        loadLeftTable();
+        loadTable();
 
         confirm2.addActionListener(new ActionListener() {
             @Override
@@ -49,20 +57,50 @@ public class ReviewArticles {
         frame.setVisible(true);
     }
 
-    private void loadRightTable() {
+
+    private void loadTable() {
+        DefaultTableModel leftModel = new DefaultTableModel(new String[]{"submissionID", "Title", "Abstract"}, 0);
+        leftTable.setModel(leftModel);
+
+
         DefaultTableModel rightModel = new DefaultTableModel(new String[]{"submissionID", "Title", "Abstract"}, 0);
         rightTable.setModel(rightModel);
 
         for (Submission submission : PublicationsController.getSubmissions()) {
             rightModel.addRow(new Object[]{submission.getSubmissionId(), submission.getTitle(), submission.getAbs()});
         }
-    }
 
-    private void loadLeftTable(){
-        DefaultTableModel leftModel = new DefaultTableModel(new String[]{"submissionID", "Title", "Abstract"}, 0);
-        leftTable.setModel(leftModel);
+        rightTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);  //Single select
+        rightTable.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                selectButton.addActionListener(new ActionListener(){
+                    public void actionPerformed(ActionEvent e){
+                        int selectedRow = rightTable.getSelectedRow();
+                        int leftRows = leftTable.getRowCount();
+                        int id = (int) rightTable.getValueAt(selectedRow, 0);
+                        String title = (String) rightTable.getValueAt(selectedRow, 1);
+                        String abs = (String) rightTable.getValueAt(selectedRow, 2);
+                         if (selectedRow != -1 && leftRows < 3){
+                            leftModel.addRow(new Object[]{id, title, abs});
+                        }
+                    }
+                });
+            }
+        });
 
-
+        leftTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        leftTable.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                deleteButton.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        int selectedRow = leftTable.getSelectedRow();
+                        if (selectedRow != -1){
+                            leftModel.removeRow(selectedRow);
+                        }
+                    }
+                });
+            }
+        });
     }
 
     private void createUIComponents(){
@@ -79,24 +117,24 @@ public class ReviewArticles {
             }
         };
 
-        rightTable.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mousePressed(MouseEvent e) {
-                JTable rightTable = (JTable) e.getSource();
-                Point point = e.getPoint();
-                int row = rightTable.rowAtPoint(point);
-                int leftRows = leftTable.getRowCount();
-                if (e.getClickCount() == 2 && rightTable.getSelectedRow() != -1 && leftRows <3){
-                    int id = (int) rightTable.getValueAt(rightTable.getSelectedRow(),0);
-                   /* for (Submission submission : PublicationsController.getSubmissions()) {
-                        model.addRow(new Object[]{submission.getSubmissionId(), submission.getTitle(), submission.getAbs()});
-                    }*/
-                   System.out.println(id);
-                   System.out.println(PublicationsController.getSubmissionsByID(id));
-
-                }
-            }
-        });
+//        rightTable.addMouseListener(new MouseAdapter() {
+//            @Override
+//            public void mousePressed(MouseEvent e) {
+//                JTable rightTable = (JTable) e.getSource();
+//                Point point = e.getPoint();
+//                int row = rightTable.rowAtPoint(point);
+//                int leftRows = leftTable.getRowCount();
+//                if (e.getClickCount() == 2 && rightTable.getSelectedRow() != -1 && leftRows <3){
+//                    int id = (int) rightTable.getValueAt(rightTable.getSelectedRow(),0);
+//                   /* for (Submission submission : PublicationsController.getSubmissions()) {
+//                        model.addRow(new Object[]{submission.getSubmissionId(), submission.getTitle(), submission.getAbs()});
+//                    }*/
+//                   System.out.println(id);
+//                   System.out.println(PublicationsController.getSubmissionsByID(id));
+//
+//                }
+//            }
+//        });
     }
 }
 
